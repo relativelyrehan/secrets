6cf4c688801df765f0fccfb0c44a8b02c67f0df5
rreel <48210765+relativelyrehan@users.noreply.github.com>
2020-05-20 19:53:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 const app = express();
-const port = 3000;
+// const port = ;
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
@@ -205,5 +205,5 @@ app.post("/login", function(req, res){
 app.post("/", function (req, res) {
     res.send("Hello World!!");
 });
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(process.env.PORT, () => console.log(`Example app listening on port port!`));
 
